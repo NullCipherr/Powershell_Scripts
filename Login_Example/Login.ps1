2497e846a653ff989b87307a2899ac45d9f6a409
NullCipherr <blendsteal@gmail.com>
2024-02-19 17:54:21 -0300

    add Login in Folder.

---
+++ Login_Example/Login.ps1
@@ -0,0 +1,20 @@
+
+function Get-Login 
+{
+    $username = Read-Host "Digite o nome de usuario"
+    $password = Read-Host "Digite a senha" -AsSecureString
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+    $UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+    
+
+    if ($username -eq 'admin' -and $UnsecurePassword -eq 'senha123') 
+    {
+        Write-Host "Login bem-sucedido ..."
+    } else 
+    {
+        Write-Host "Usuario ou senha incorretos!"
+    }
+}
+
+# Chama a função Get-Login
+Get-Login
